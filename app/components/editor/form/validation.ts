73368e4816ccbed29512d6f50e385ec534ce9a81
Okan Ay <okanay@Okan-MacBook-Pro-2.local>
2025-04-10 15:15:13 +0300

    blog 27

--- app/components/editor/form/validation.ts
+++ app/components/editor/form/validation.ts
@@ -1,5 +1,43 @@
 import { z } from "zod";
 
+const imageUrlSchema = z
+  .string()
+  .url()
+  .refine(
+    async (url) => {
+      // Daha önce başlatılmış bir isteği iptal etmek için controller
+      let controller = new AbortController();
+
+      try {
+        // İstek için zaman aşımı süresi (3 saniye)
+        const timeoutId = setTimeout(() => controller.abort(), 3000);
+
+        const response = await fetch(url, {
+          method: "HEAD",
+          signal: controller.signal, // AbortController sinyalini ekle
+        });
+
+        // Zaman aşımı zamanlayıcısını temizle
+        clearTimeout(timeoutId);
+
+        return (
+          response.ok &&
+          response.headers.get("content-type")?.startsWith("image/")
+        );
+      } catch (error) {
+        // AbortError olup olmadığını kontrol et
+        if (error.name === "AbortError") {
+          console.log("Fetch işlemi zaman aşımına uğradı veya iptal edildi");
+        }
+        return false;
+      } finally {
+        // Controller'ı temizle
+        controller = null;
+      }
+    },
+    { message: "URL erişilebilir bir görsel dosyasına işaret etmelidir." },
+  );
+
 export const formSchema = z.object({
   seoTitle: z
     .string()
@@ -18,12 +56,7 @@ export const formSchema = z.object({
       "SEO açıklaması en az 40 karakter olmalıdır. Bu alan boş bırakılamaz.",
     )
     .max(160, "SEO açıklaması en fazla 160 karakter olabilir."),
-  seoImage: z
-    .string()
-    .min(
-      10,
-      "Sosyal medya görseli en az 10 karakter olmalıdır. Bu alan boş bırakılamaz.",
-    ),
+  seoImage: imageUrlSchema,
   blogTitle: z
     .string()
     .min(
@@ -38,12 +71,12 @@ export const formSchema = z.object({
       "Kart açıklaması en az 10 karakter olmalıdır. Bu alan boş bırakılamaz.",
     )
     .max(120, "Kart açıklaması en fazla 120 karakter olabilir."),
-  blogImage: z.string(),
+  blogImage: imageUrlSchema,
   categories: z.array(z.string()).min(1, "En az 1 kategori seçilmelidir."),
   tags: z.array(z.string()).optional(),
   language: z.string().min(1, "Blog dili seçilmelidir."),
   featured: z.boolean().default(false),
-  readTime: z.number().min(1, "Okuma süresi 1'den küçük olamaz."),
+  readTime: z.number().min(1, "Okuma dakikasi en az 1 dakika olmalıdır."),
   status: z.string().min(1, "Blog durumu seçilmelidir."),
   isCanonical: z.boolean().default(true),
 });
