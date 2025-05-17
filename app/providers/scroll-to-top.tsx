c7ea8181c72a54af2a19376a5ab66418c444eb86
Okan Ay <okanay@Okan-MacBook-Pro-2.local>
2025-05-17 12:49:02 +0300

    migration v2

--- app/providers/scroll-to-top.tsx
+++ app/providers/scroll-to-top.tsx
@@ -4,14 +4,14 @@ import { useEffect } from "react";
 export function ScrollToTop() {
   const location = useLocation();
 
-  useEffect(() => {
-    // Sayfa yüklendikten sonra en üste kaydır
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant", // veya "smooth" için yumuşak kaydırma
-    });
-  }, [location.pathname]); // location.pathname değiştiğinde çalışır
+  // useEffect(() => {
+  //   // Sayfa yüklendikten sonra en üste kaydır
+  //   window.scrollTo({
+  //     top: 0,
+  //     left: 0,
+  //     behavior: "instant", // veya "smooth" için yumuşak kaydırma
+  //   });
+  // }, [location.pathname]); // location.pathname değiştiğinde çalışır
 
   return null;
 }
